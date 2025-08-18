f38e0e70286998b4e91c7cb5a88bfe53cdb37baa
vuhunglongp <vuhunglongp@student.unimelb.edu.au>
2025-08-18 16:01:56 +1000

    check existing email controllers/auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,6 +5,13 @@ const prisma = new PrismaClient();
 exports.register = async (req, res) => {
   const { email, password, role } = req.body;
   try {
+    // Check if email is not in the database
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) {
+      return res.status(409).json({ error: 'This email address has been linked to an existing account' });
+    }
+
+    // Hash password in create user
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await prisma.user.create({
       data: {
