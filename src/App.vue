7eadd9a832d4268ebda9facfd557eece2afad29a
YongjiangSong <yonsong@student.unimelb.edu.au>
2025-08-26 23:59:30 +1000

    LogIn_page: Final implementation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
 <template>
   <div id="app">
     <RouterView />
   </div>
 </template>
 
-<script setup>
-import { RouterView } from 'vue-router'
-</script>
-
-<style>
+<style scoped>
 #app {
-  margin: 0;
-  padding: 0;
   width: 100%;
   min-height: 100vh;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
-}
-
-* {
-  box-sizing: border-box;
-}
-
-body {
-  margin: 0;
-  padding: 0;
 }
 </style>
