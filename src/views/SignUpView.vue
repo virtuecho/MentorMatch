fd06781a1ad195a22a4753b34521b876209805cf
YongjiangSong <yonsong@student.unimelb.edu.au>
2025-08-26 23:36:40 +1000

    SignUp_page: Final implementation - Add view components

---
+++ src/views/SignUpView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="signup-view">
+    <HeaderComponent />
+    <SignUpViewContent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from '@/components/HeaderComponent.vue'
+import SignUpViewContent from '@/components/SignUpViewContent.vue'
+
+export default {
+  name: 'SignUpView',
+  components: {
+    HeaderComponent,
+    SignUpViewContent
+  }
+}
+</script>
+
+<style scoped>
+.signup-view {
+  width: 100%;
+  min-height: 100vh;
+  background: #F7FAFC;
+}
+</style>
