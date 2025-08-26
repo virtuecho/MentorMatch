9ea0a086f7a89e9fb9fcc8e954ecb2dac1758853
vuhunglongp <vuhunglongp@student.unimelb.edu.au>
2025-08-26 22:26:46 +1000

    setup CORS

---
+++ src/config/cors.js
@@ -0,0 +1,16 @@
+const allowedOrigins = [
+  'http://localhost:3000'
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+};
+
+module.exports = { corsOptions };
