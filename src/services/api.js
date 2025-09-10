74adab455093ea6c6f458395891637ff80a0604a
xie10 <xie10@student.unimelb.edu.au>
2025-09-10 14:30:29 +1000

    Fix blank page issue

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const BASE_URL = process.env.VUE_APP_API_BASE_URL
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
 const API = axios.create({
     baseURL: BASE_URL,
     headers: {
