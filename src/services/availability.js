96e55c48b2d0a44c9ef243c9416facde186d3dcb
vuhunglongp <vuhunglongp@student.unimelb.edu.au>
2025-09-23 00:38:43 +1000

    add all availability endpoints

--- src/services/availability.js
+++ src/services/availability.js
@@ -18,8 +18,18 @@ export const createAvailability = ({
 
     return API.post("/availability/slots", payload);
 }
-    
+
+// User view their own availability slots
+export const getMyAvailability = () => {
+    const res = API.get("/availability/slots");
+    return res.data;
+}
 
 // Mentee view availability slots given mentor id
-export const getMentorAvailability = (mentorID) =>
+export const getMentorAvailability = ({ mentorID }) =>
     API.get("/mentor/:mentorId", { mentorID });
+
+// Delete your availability slot
+export const deleteAvailability = ({ slotId }) => {
+    API.delete("/slots/:slotId", { slotId })
+}
