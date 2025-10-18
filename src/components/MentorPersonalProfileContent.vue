25aa2255dbc77f5817fc50e3e89768757edab41d
songyongjiang <yonsong@student.unimelb.edu.au>
2025-10-18 20:56:00 +1100

    bug fix

--- src/components/MentorPersonalProfileContent.vue
+++ src/components/MentorPersonalProfileContent.vue
@@ -329,7 +329,7 @@ export default {
       } catch (error) {
         console.error('Failed to load mentor profile:', error);
       }
-    }
+    },
     goBack() {
       this.$router.go(-1);
     },
